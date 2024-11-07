ae7165222876339e9bdfdb31d519278a2ebe0e5d
william0814 <king-master90@hotmail.com>
2024-11-07 22:29:03 +0100

    sintaxis

---
+++ inicio.py
@@ -0,0 +1,29 @@
+'''x = "el valor de (a+b)*c es: "
+
+a, b, c = 4, 3, 2
+
+d = (a+b)*c
+imprimir = True
+if imprimir :
+    print(x, d)'''
+ # esto es un comentario
+
+if True:
+    print("hola")
+    suma = 2+2 
+    print (suma)
+    if suma == 4:
+        suma= (5+5)
+        print(suma)
+
+def funcion(a, b, c):
+    return a+b+c
+    
+d = funcion(10,23,3)
+def mi_funcion( x,y ):
+    
+    return x+ y
+
+
+
+
